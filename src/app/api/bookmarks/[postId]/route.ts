2266a5121c1921f4c81abb28970e1f1089643cc5
Tobias <tobias.bp2004@gmail.com>
2026-04-08 10:34:29 -0300

    fix: lint

--- src/app/api/bookmarks/[postId]/route.ts
+++ src/app/api/bookmarks/[postId]/route.ts
@@ -1,6 +1,6 @@
-import { getServerSession } from 'next-auth';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { API_BASE_URL } from '@/utils/consts/api';
 
--- src/app/api/bookmarks/route.ts
+++ src/app/api/bookmarks/route.ts
@@ -1,5 +1,5 @@
-import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
+import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { API_BASE_URL } from '@/utils/consts/api';
 
