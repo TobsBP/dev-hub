e60cfe555660d84a124b97f767aae7ac93f25039
Joao07912 <joaofraga078@gmail.com>
2026-04-08 20:19:06 -0300

    refactor: rename doLogin to setup and streamline test setup process

--- cypress/e2e/postcard.cy.ts
+++ cypress/e2e/postcard.cy.ts
@@ -1,5 +1,5 @@
 describe('Componente PostCard', () => {
-  const doLogin = () => {
+  const setup = () => {
     cy.session('postcard-user', () => {
       cy.visit('/login');
       cy.get('input[name="email"]').type('teste1@email.com');
@@ -7,17 +7,14 @@ describe('Componente PostCard', () => {
       cy.get('button[type="submit"]').click();
       cy.url({ timeout: 10000 }).should('not.include', '/login');
     });
-  };
-
-  beforeEach(() => {
-    doLogin();
     cy.visit('/');
     cy.get('article', { timeout: 10000 }).should('be.visible');
-  });
+  };
 
   // --- Renderização do card ---
 
   it('PC-001 deve exibir o avatar com inicial ou imagem do autor', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.get('div.w-9.h-9.rounded-full')
         .should('be.visible')
@@ -30,6 +27,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-002 deve exibir o nome ou email do autor', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.get('p.text-sm.font-medium.text-white')
         .should('be.visible')
@@ -41,6 +39,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-003 deve exibir a data de criação do post', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.get('p.text-xs.text-zinc-500')
         .should('be.visible')
@@ -52,6 +51,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-004 deve exibir o badge com o tipo do post', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.get('span.rounded-full.text-zinc-400')
         .should('be.visible')
@@ -63,6 +63,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-005 deve exibir o conteúdo do post', () => {
+    setup();
     cy.get('article').first().find('div.px-4.py-4')
       .should('be.visible')
       .invoke('text')
@@ -74,22 +75,23 @@ describe('Componente PostCard', () => {
   // --- Curtidas ---
 
   it('PC-006 deve exibir o botão de curtir com contador', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.get('button').contains(/^\d+$/).should('exist');
     });
   });
 
-  
-
   // --- Comentários ---
 
   it('PC-007 deve exibir o botão de comentários com contador', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.contains('button', /comentário/).should('be.visible');
     });
   });
 
   it('PC-008 deve abrir a área de comentários ao clicar no botão', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.contains('button', /comentário/).click();
       cy.get('input[placeholder*="Comentar"]').should('be.visible');
@@ -97,6 +99,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-009 deve fechar a área de comentários ao clicar novamente no botão', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.contains('button', /comentário/).click();
       cy.get('input[placeholder*="Comentar"]').should('be.visible');
@@ -106,6 +109,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-010 deve manter o botão publicar desabilitado com texto menor que 2 caracteres', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.contains('button', /comentário/).click();
       cy.get('input[placeholder*="Comentar"]').type('a');
@@ -114,6 +118,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-011 deve habilitar o botão publicar com texto de 2 ou mais caracteres', () => {
+    setup();
     cy.get('article').first().within(() => {
       cy.contains('button', /comentário/).click();
       cy.get('input[placeholder*="Comentar"]').type('ok');
@@ -124,6 +129,7 @@ describe('Componente PostCard', () => {
   // --- Tipos de post ---
 
   it('PC-012 deve renderizar post do tipo "code" com syntax highlighter', () => {
+    setup();
     cy.get('article').contains('span.rounded-full', /[Cc]ódigo|code/i)
       .closest('article')
       .find('pre')
@@ -131,6 +137,7 @@ describe('Componente PostCard', () => {
   });
 
   it('PC-013 deve renderizar post do tipo "question" com destaque visual', () => {
+    setup();
     cy.get('article').contains('span.rounded-full', /[Pp]ergunta|question/i)
       .closest('article')
       .find('div.bg-amber-950\\/30')
